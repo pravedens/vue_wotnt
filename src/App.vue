c28ad137771bb474b69b8072eb6b2a73252fc9ba
praveden <pravedens@gmail.com>
2022-11-06 13:02:40 +0500

    Add banner and toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,63 @@
 <template>
-  <h1>Hello Vue</h1>
-  <router-view/>
+
+    <v-layout>
+      <v-app-bar
+          color="teal-darken-4"
+          image="https://picsum.photos/1920/1080?random"
+      >
+        <template v-slot:image>
+          <v-img
+              gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>WoTnt</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-navigation-drawer
+          expand-on-hover
+          rail
+      >
+        <v-list>
+          <v-list-item
+              prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+              title="МРО ЦХВЕ"
+              subtitle="Слово Истины"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <v-card height="200px"></v-card>
+        <router-view/>
+      </v-main>
+    </v-layout>
+
+
 </template>
 
 <style>
