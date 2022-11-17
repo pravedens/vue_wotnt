2ec45e85d5f2bcbd750e3939fa73d7716374f735
praveden <pravedens@gmail.com>
2022-11-17 19:28:36 +0500

    Создание страницы объявления

--- src/components/Gospel/ListGospel.vue
+++ src/components/Gospel/ListGospel.vue
@@ -1,10 +1,98 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>ListGospel</h1>
-      </v-flex>
-    </v-layout>
+  <v-toolbar
+      color="">
+    <v-toolbar-title>Список проповедей</v-toolbar-title>
+  </v-toolbar>
+  <v-container
+      fluid
+  >
+    <v-row dense>
+      <v-col
+          v-for="item of items"
+          :key="item.id"
+          :cols="item.flex"
+
+      >
+        <v-card
+      max-width="400"
+      class="mx-auto mb-5 elevation-12"
+  >
+
+    <!-- <v-system-bar
+      color="pink darken-2"
+      dark
+    >
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-window-minimize</v-icon>
+
+      <v-icon>mdi-window-maximize</v-icon>
+
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar>
+
+    <v-app-bar
+      dark
+      color="pink"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My Music</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar> -->
+
+    <v-container>
+      <v-row dense>
+        <v-col
+            cols="12"
+        >
+          <v-card
+              color="#1F7087"
+              theme="dark"
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h5">
+                  {{ item.title }}
+                </v-card-title>
+
+                <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+
+                <v-card-actions>
+                  <v-btn
+                      class="ml-2"
+                      variant="outlined"
+                      size="small"
+                      :to="'/MainGospel/' + item.id"
+                  >
+                    Открыть
+                  </v-btn>
+                </v-card-actions>
+              </div>
+
+              <v-avatar
+                  class="ma-3"
+                  size="150"
+                  rounded="0"
+              >
+                <v-img
+                    :src="item.imageSrc"
+                    cover
+                ></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -12,7 +100,38 @@
 export default {
   name: "ListGospel",
   data () {
-    return {}
+    return {
+      items: [
+        {
+          title: 'First ad',
+          description: 'Hello i am description_1',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '123'
+        },
+        {
+          title: 'Second ad',
+          description: 'Hello i am description_2',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          id: '1234'
+        },
+        {
+          title: 'Third ad',
+          description: 'Hello i am description_3',
+          promo: true,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          id: '12345'
+        },
+        {
+          title: 'For ad',
+          description: 'Hello i am description_4',
+          promo: true,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          id: '123456'
+        },
+      ]
+    }
   }
 }
 </script>
--- src/components/Gospel/MainGospel.vue
+++ src/components/Gospel/MainGospel.vue
@@ -1,19 +1,81 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>MainGospel</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-card
+      :loading="loading"
+      class="mx-auto my-12"
+      max-width="1200px"
+  >
+    <template v-slot:loader="{ isActive }">
+      <v-progress-linear
+          :active="isActive"
+          color="deep-purple"
+          height="4"
+          indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+        cover
+        height="350px"
+        src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+    ></v-img>
+
+    <v-card-item>
+      <v-card-title>Cafe Badilico</v-card-title>
+
+      <v-card-subtitle>
+        <v-icon
+            color="error"
+            icon="mdi-fire-circle"
+            size="small"
+        ></v-icon>
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-card-text>
+      <v-row
+          align="center"
+          class="mx-0"
+      >
+      </v-row>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+          variant="flat"
+          color="secondary"
+          @click="reserve"
+      >
+        Edit
+      </v-btn>
+      <v-btn
+          color="deep-purple-lighten-2"
+          variant="text"
+          @click="reserve"
+      >
+        Buy
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: "MainGospel",
   data () {
-    return {}
-  }
+    return {
+      loading: false,
+      selection: 1,
+    }
+  },
+  methods: {
+    reserve () {
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
+    },
+  },
 }
 </script>
 
