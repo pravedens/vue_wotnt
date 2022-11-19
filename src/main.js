15669d16d6c0402aca40d84a3f953fa879610b46
praveden <pravedens@gmail.com>
2022-11-19 16:48:04 +0500

    Настройка firebase

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from "firebase/compat"
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD2pG-_ktyoNEnmpjX1_8JMpnHxX35NtZk",
+    authDomain: "wotnt-35e5e.firebaseapp.com",
+    projectId: "wotnt-35e5e",
+    storageBucket: "wotnt-35e5e.appspot.com",
+    messagingSenderId: "1000125921850",
+    appId: "1:1000125921850:web:5838c92de2335056461303"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 loadFonts()
 
